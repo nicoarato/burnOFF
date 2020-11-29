fc7f74ba5a56ca887bf049e5e0a118d61ae4853b
Nico Arato <nico.arato@gmail.com>
2020-11-29 13:54:49 -0300

    Listo para produccion

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -5,31 +5,201 @@ Chart.defaults.global.defaultFontColor = '#858796';
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
+    type: 'doughnut',
+    data: {
+        labels: ["Direct", "Referral"],
+        datasets: [{
+            data: [95, 5],
+            backgroundColor: ['#1cc88a', '#858796'],
+            hoverBackgroundColor: ['#17a673', '#858796'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
     },
-    legend: {
-      display: false
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
     },
-    cutoutPercentage: 80,
-  },
+});
+
+//Second Pie Chart Example
+var ctx2 = document.getElementById("mySecondPieChart");
+var myPieChart = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ["Horas frente al monitor", "Horas de descanso"],
+        datasets: [{
+            data: [85, 15],
+            backgroundColor: ['#e74a3b', '#858796'],
+            hoverBackgroundColor: ['#e74a3b', '#858796'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+});
+
+// Third Pie Chart Example
+var ctx3 = document.getElementById("myThirdPieChart");
+var myPieChart = new Chart(ctx3, {
+
+    type: 'doughnut',
+    data: {
+        labels: ["Litros de agua consumidos", "Litros de agua sin consumir"],
+        datasets: [{
+            data: [65, 35],
+            backgroundColor: ['#36b9cc', '#858796'],
+            hoverBackgroundColor: ['#36b9cc', '#858796'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+
+});
+
+// Four Pie Chart Example
+var ctx4 = document.getElementById("myFourPieChart");
+var myPieChart = new Chart(ctx4, {
+
+    type: 'doughnut',
+    data: {
+        labels: ["Tareas completadas", "Tareas sin completar"],
+        datasets: [{
+            data: [65, 35],
+            backgroundColor: ['#4e73df', '#858796'],
+            hoverBackgroundColor: ['#4e73df', '#858796'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+
+});
+
+
+// Five Pie Chart Example
+var ctx5 = document.getElementById("myFivePieChart");
+var myPieChart = new Chart(ctx5, {
+
+    type: 'doughnut',
+    data: {
+        labels: ["Herramientas de gestion", "Otra"],
+        datasets: [{
+            data: [50, 50],
+            backgroundColor: ['#f6c23e', '#858796'],
+            hoverBackgroundColor: ['#f6c23e', '#858796'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+
+});
+
+
+// Third Pie Chart Example
+var ctx3 = document.getElementById("mySixPieChart");
+var myPieChart = new Chart(ctx3, {
+
+    type: 'doughnut',
+    data: {
+        labels: ["Horas trabajadas", "Horas restantes"],
+        datasets: [{
+            data: [100],
+            backgroundColor: ['#1cc88a'],
+            hoverBackgroundColor: ['#5a5c69'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+
 });
